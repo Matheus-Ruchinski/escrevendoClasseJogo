385309e2cbcd684ee4255eb5f3e74bed919356fe
Matheus-Ruchinski <matheus.sperkoski01@gmail.com>
2024-10-02 21:21:40 -0300

    Implementacao de melhorias no codigo fote

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 class heroi{
-    // método cronstrutor a qual recebe os parâmetros e tem como padrão a tipo Guerreio, caso o usuário não infornme
     constructor(nome, idade, tipo = "Guerreiro"){
-        // Declarando variáveis e atribuido valor a elas 
         this.nome = nome
         this.idade = idade
-        this.tipo = tipo
-        this.ataque = ""
-        // O método construtor chama a função tipo de ataque para atribuir o ataque que pode ser realizado pela classe(Guerreiro, Mago, Monge, Ninja)
-        this.tipoAtaque(tipo)
+        this.tipo = this.capitalize(tipo)
+        this.ataque = this.definirTipoAtaque()
     }
-    tipoAtaque(tipo){
-        // Estrutura de decisão, irá verificar a classe do herói
-            if (tipo == "Guerreiro"){
-                this.ataque = "Espada"
-                // quando entrar em um dos if, será atribuido o valor na variável ataque
-                } else if (tipo == "Mago"){
-                    this.ataque = "Magia"
-                    } else if (tipo == "Monge"){
-                        this.ataque = "Artes Marciais"
-                        } else if (tipo == "Ninja"){
-                            this.ataque = "Shuriken"
-                            } else {
-                                // Caso o usuário digite uma classe errado, entrara no else e informará ao usuário sobre o erro
-                                console.log("A classe escolhida não existe, Tente novamente!")
-                                this.ataque = "Ataque indefinido"
-                                }
-        return this.ataque
+    capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    }
+    definirTipoAtaque(){
+        switch (this.tipo) {
+            case "Guerreiro":
+                return "Espada";
+            case "Mago":
+                return "Magia";
+            case "Monge":
+                return "Artes Marciais";
+            case "Ninja":
+                return "Shuriken";
+            default:
+                console.log("A classe escolhida não existe, tente novamente!");
+                return "Ataque indefinido";
+        }
     }
     atacar(){
             console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
     }
 }
-// Instanciando objetos na classe heroi
 let garen = new heroi("Garen", 18)
 garen.atacar()
-let ahri = new heroi("Ahri", 21, "Mago")
+let ahri = new heroi("Ahri", 21, "MAGO")
 ahri.atacar()
-let leeSin = new heroi("Lee Sin", 30, "Monge")
+let leeSin = new heroi("Lee Sin", 30, "MoNgE")
 leeSin.atacar()
 let zed = new heroi("Zed", 25, "ninja")
 zed.atacar()
